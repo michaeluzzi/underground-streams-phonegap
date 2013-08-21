37ac031eadba2eb9dd9989c38a3642482fe0726f
Michael Uzzi <mjuzzi@gmail.com>
2013-08-21 17:40:36 -0400

    submit form

--- www/js/index.js
+++ www/js/index.js
@@ -81,14 +81,14 @@ var app = {
 // current GPS coordinates
 //
 var onPositionSuccess = function(position) {
-    /*alert('Latitude: '          + position.coords.latitude          + '\n' +
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
           'Longitude: '         + position.coords.longitude         + '\n' +
           'Altitude: '          + position.coords.altitude          + '\n' +
           'Accuracy: '          + position.coords.accuracy          + '\n' +
           'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
           'Heading: '           + position.coords.heading           + '\n' +
           'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');*/
+          'Timestamp: '         + position.timestamp                + '\n');
     
     // real coordinates
     var str = "http://underground-streams-dev.elasticbeanstalk.com/api/nearbyStations?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude;      
