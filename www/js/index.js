b8c999edba115aa877fb89f5cb74fb1a134a5b80
Michael Uzzi <mjuzzi@gmail.com>
2013-08-22 17:11:13 -0400

    submit form

--- www/js/index.js
+++ www/js/index.js
@@ -100,7 +100,8 @@ var onPositionSuccess = function(position) {
         function(nearbyStations) {
         	jsonObj.nearbyStations = nearbyStations;
         	alert("nearest station " + jsonObj.nearbyStations[0].STOP_NAME);
-        	jsonObj.lines = new Array();
+        	var lines = new Array();
+        	//jsonObj.lines = new Array();
         	var submitLineDropdown = document.getElementById("submit-line");
         	for (var i = 0; i < jsonObj.nearbyStations[0].Routes_ALL.length; i++)
         	{
@@ -115,11 +116,11 @@ var onPositionSuccess = function(position) {
         			function(stations) {
         				//alert(line);
         				//alert(stations.length);
-        				jsonObj.lines.push({
-        					"line" : "A",			
+        				/*jsonObj.*/lines.push({
+        					"line" : line,			
         					"stations" : stations
         				});
-        				alert(jsonObj.lines[i].line);
+        				alert(/*jsonObj.*/lines[i].line);
         			}
         		);
         		//alert(jsonObj.lines[i].line);
