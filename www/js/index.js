eea82c32b667983868e558a1808eec1ee0feb1e6
Michael Uzzi <mjuzzi@gmail.com>
2013-08-29 17:56:05 -0400

    browse

--- www/js/index.js
+++ www/js/index.js
@@ -92,14 +92,14 @@ var app = {
 // current GPS coordinates
 //
 var onPositionSuccess = function(position) {
-    /*alert('Latitude: '          + position.coords.latitude          + '\n' +
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
           'Longitude: '         + position.coords.longitude         + '\n' +
           'Altitude: '          + position.coords.altitude          + '\n' +
           'Accuracy: '          + position.coords.accuracy          + '\n' +
           'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
           'Heading: '           + position.coords.heading           + '\n' +
           'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');*/
+          'Timestamp: '         + position.timestamp                + '\n');
     
     
     // Find nearby stations
